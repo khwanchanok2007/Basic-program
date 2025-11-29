2169e3d831ceba9cc4fa145beb2a2ebebd5e4e83
@˚.˙ ᴋʜᴡᴀɴᴄʜᴀɴᴏᴋ 𖦹  ˙𓃠˚ <khwanchanoktafai@gmail.com>
2025-11-29 16:51:40 +0700

    Add files via upload

---
+++ lab3.5.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+struct Order {
+    int itemId;
+    float unitPrice;
+    int quantity;
+};
+
+int main() {
+    int N, i;
+    float itemTotal = 0.0;
+    float grandTotal = 0.0;
+    float SHIPPING_FEE = 50.0;
+    float discount = 0.0;
+
+    if (scanf("%d", &N) != 1) {
+        return 1;
+    }
+
+    struct Order orders[N];
+
+    for (i = 0; i < N; i++) {
+
+        if (scanf("%d %f %d", &orders[i].itemId, &orders[i].unitPrice, &orders[i].quantity) != 3) {
+            return 1;
+        }
+
+        itemTotal = (orders[i].unitPrice * orders[i].quantity) + SHIPPING_FEE;
+
+        if (itemTotal >= 500.0) {
+            discount = itemTotal * 10 / 100;
+            itemTotal -= discount;
+        }
+
+        grandTotal += itemTotal;
+    }
+    
+    printf("Grand Total: %.2f\n", grandTotal);
+
+    return 0;
+}
