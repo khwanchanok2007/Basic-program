881c904bad49a9986ca26931d92d53b540f16239
@˚.˙ ᴋʜᴡᴀɴᴄʜᴀɴᴏᴋ 𖦹  ˙𓃠˚ <khwanchanoktafai@gmail.com>
2025-11-29 17:09:35 +0700

    Add files via upload

---
+++ lab3.9.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int N_DAYS, day;
+    float initialBudget, remainingBudget, dailySpend;
+
+    if (scanf("%f %d", &initialBudget, &N_DAYS) != 2) {
+        return 1;
+    }
+
+    remainingBudget = initialBudget;
+
+    printf("Day | Spend | Remaining\n");
+    printf("----------------------------------\n");
+
+    for (day = 1; day <= N_DAYS; day++) {
+        if (remainingBudget >= 500.00) {
+            dailySpend = 100.00;
+        } else if (remainingBudget >= 100.00) {
+            dailySpend = 50.00;
+        } else if (remainingBudget > 0.0) {
+            dailySpend = 20.0;
+        } else {
+            dailySpend = 0.0;
+        }
+
+        if (dailySpend > remainingBudget) {
+            dailySpend = remainingBudget;
+        }
+
+        remainingBudget -= dailySpend;
+        printf("%d | %.2f | %.2f\n", day, dailySpend, remainingBudget);
+    }
+
+    printf("----------------------------------\n");
+    printf("Final Budget: %.2f\n", remainingBudget);
+    return 0;
+}
