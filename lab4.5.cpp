9f45c0d11e799528c261b95117017e775b3a050e
@˚.˙ ᴋʜᴡᴀɴᴄʜᴀɴᴏᴋ 𖦹  ˙𓃠˚ <khwanchanoktafai@gmail.com>
2025-11-29 17:43:55 +0700

    Add files via upload

---
+++ lab4.5.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int SIZE = 6;
+    int data[SIZE];
+    int total_sum = 0;
+    int i;
+
+    for (i = 0; i < SIZE; i++)
+    {
+        printf("Enter integer number %d: ", i + 1);
+        scanf("%d", &data[i]);
+    }
+
+    for (i = 0; i < SIZE; i++)
+    {
+        total_sum += data[i];
+    }
+
+    printf("\n--- SUMMATION REPORT ---\n");
+    printf("Recorded Numbers: ");
+    for (i = 0; i < SIZE; i++)
+    {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+    printf("Total Sum of Numbers: %d\n", total_sum);
+
+    return 0;
+}
