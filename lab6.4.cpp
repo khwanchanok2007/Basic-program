1cef3c3e4ab37871368ae6d219f078a9d5463e4b
@˚.˙ ᴋʜᴡᴀɴᴄʜᴀɴᴏᴋ 𖦹  ˙𓃠˚ <khwanchanoktafai@gmail.com>
2025-11-30 16:22:14 +0700

    Add files via upload

---
+++ lab6.4.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// calculate Sum Product function
+void calculateSumProduct(int num1, int num2, int *sum_ptr, int *prod_ptr)
+{
+    *sum_ptr = num1 + num2;
+    *prod_ptr = num1 * num2;
+}
+
+int main()
+{
+    int valA, valB;
+    int resultSum, resultProduct;
+
+    printf("Enter first integer (Val A) : ");
+    scanf("%d", &valA);
+
+    printf("Enter second integer (Val B) : ");
+    scanf("%d", &valB);
+
+    calculateSumProduct(valA, valB, &resultSum, &resultProduct);
+
+    printf("\n--- CALCULATION REPORT ---\n");
+    printf("Input Values: A = %d, B = %d\n", valA, valB);
+    printf("--------------------------------------\n");
+    printf("Result Sum (A + B): %d\n", resultSum);
+    printf("Result Product (A * B): %d\n", resultProduct);
+}
